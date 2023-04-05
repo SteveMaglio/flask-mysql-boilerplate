f000163207d7857bc6aa540809fd114ae722377c
stephenmaglio <magliocchino.s@northeastern.edu>
2023-04-05 11:20:07 -0400

    please stay!!!!!

--- flask-app/src/products/products.py
+++ flask-app/src/products/products.py
@@ -58,3 +58,53 @@ def get_most_pop_products():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+# Get all the products from the database
+@products.route('/products', methods=['GET'])
+def get_products():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('SELECT id, product_code, product_name, list_price FROM products')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+# get the top 5 products from the database
+@products.route('/categories', methods=['GET'])
+def get_all_categories():
+    query = '''
+        SELECT DISTINCT category AS label, category as value
+        from products
+        where category is not null
+        order by category
+    '''
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    
+    json_data = []
+    
+       # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
